2f058890fde516ae5b1324c1b31eb83dd7a0e06b
hemant <122742470+hemantsm20@users.noreply.github.com>
2023-09-13 16:14:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -126,10 +126,15 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(port, function() {
-  console.log("Server started on port" + port);
-});
+// app.listen(port, function() {
+//   console.log("Server started on port" + port);
+// });
 
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
 
 
 
